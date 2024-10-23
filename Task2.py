6d427ab078dcd8a99b40a861cadc795bd4186887
erkaufman <erkaufman6@gmail.com>
2024-10-23 16:23:59 -0400

    Task 2

---
+++ Task2.py
@@ -0,0 +1,21 @@
+import arcpy, sys
+
+# Set workspace
+arcpy.env.workspace = "V:\\ENV859_PS4\\Data"
+
+# Enable existing ArcPy outputs to be overwritten
+arcpy.env.overwriteOutput = True
+
+# Path to Roads.shp
+road_path = "Roads.shp"
+road_types_string = "0;201;203"
+road_types_list = road_types_string.split(';')
+
+# iterate through road types
+for road in road_types_string.split(';'):
+    # Set local variables
+    where_clause = f'ROAD_TYPE = {road}'
+    out_feature_class = f"V:\\ENV859_PS4\\Scratch\\roads_{road}.shp"
+
+    # Execute Select
+    arcpy.Select_analysis(road_path, out_feature_class, where_clause)
