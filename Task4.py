a94329aa399141c8cf863f522ac278de1288ee51
erkaufman <erkaufman6@gmail.com>
2024-10-29 17:32:59 -0400

    Making task 4 a gis tool script

--- Task4.py
+++ Task4.py
@@ -3,26 +3,26 @@ import sys, os, arcpy
 arcpy.env.overwriteOutput= True
 
 # Set input variable 
-dataset = 'V:/ARGOSTracking/Scratch/ARGOSTrack.shp'   #arcpy.GetParameterAsText(0)
+dataset = arcpy.GetParameterAsText(0)   #'V:/ARGOSTracking/Scratch/ARGOSTrack.shp'
 
 # Create describe object
 desc = arcpy.Describe(dataset)
 
 # Message what the catalogPath is
-print(f"The CatalogPath is: {desc.catalogPath}") # arcpy.AddMessage()
+arcpy.AddMessage(f"The CatalogPath is: {desc.catalogPath}")
 
 # Message the extent
-print(f"XMin: {round(desc.extent.XMin, 1)}")
-print(f"XMax: {round(desc.extent.XMax, 1)}")
-print(f"YMin: {round(desc.extent.YMin, 1)}")
-print(f"YMax: {round(desc.extent.YMax, 1)}")
+arcpy.AddMessage(f"XMin: {round(desc.extent.XMin, 1)}")
+arcpy.AddMessage(f"XMax: {round(desc.extent.XMax, 1)}")
+arcpy.AddMessage(f"YMin: {round(desc.extent.YMin, 1)}")
+arcpy.AddMessage(f"YMax: {round(desc.extent.YMax, 1)}")
 
 # Check dataset type and execute command accordingly
 if desc.datasetType == 'FeatureClass':
-    print(f"ShapeType: {desc.shapeType}") # arcpy.AddWarning()
+    arcpy.AddWarning(f"ShapeType: {desc.shapeType}") # arcpy.AddWarning()
 if desc.datasetType == 'RasterDataset':
-    print(f"Format: {desc.format}\
+    arcpy.AddWarning(f"Format: {desc.format}\
           # of rows: {desc.height}\
           # of cols: {desc.width}")
 else:
-    print()
+    arcpy.AddError(f"The data set type is: {desc.datasetType}")
