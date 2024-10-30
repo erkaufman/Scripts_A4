5fe02c540876fe0596103a97c05255148456c1ee
erkaufman <erkaufman6@gmail.com>
2024-10-30 12:43:14 -0400

    Script for python tool to evaluate if point is within a feature layer

--- Task5.py
+++ Task5.py
@@ -3,11 +3,22 @@ import sys, os, arcpy
 arcpy.env.overwriteOutput= True
 
 # Set input variables
-FeatureClass = 'V:/ARGOSTracking/Scratch/ARGOSTrack.shp'   #arcpy.GetParameterAsText(0)
-Fieldname = '' #arcpy.GetParameterAsText(1) 
+FeatureClass = arcpy.GetParameterAsText(0) # 'V:\\ENV859_PS4\\Data\\TriCounties.shp'   
+Fieldname = arcpy.GetParameterAsText(1) # 'CO_NAME' 
 
 # Create a point object
-# Construct a point object from the feature class
 obsPoint = arcpy.Point()
 obsPoint.X = 590000
 obsPoint.Y = 230000
+
+# Search cursor for designated feature class
+rows = arcpy.da.SearchCursor(FeatureClass, ['Shape@', Fieldname])
+
+# iteraete through features in feature class
+for row in rows:
+    recShape = row[0]
+    # Does point fall within the record's shape?
+    if recShape.contains(obsPoint) == True:
+        # Attribute value of the field specified by the user
+        uservalue = row[1]
+        arcpy.AddMessage(f'The point is within {uservalue} county.') 
